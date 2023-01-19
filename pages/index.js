27d0b0b597f075c3fcd38578414531098335112c
Alfredo Arriola Jr <73959568+alfredoarriolajr@users.noreply.github.com>
2023-01-18 23:36:01 -0800

    add

--- pages/index.js
+++ pages/index.js
@@ -57,7 +57,7 @@ export default function Home() {
                         onClick={() => {
                             supabase
                                 .from('favorites')
-                                .upsert({ favorite: addFavorite })
+                                .insert({ favorite: addFavorite })
                                 .then((res) => {
                                     console.log(res);
                                 })
